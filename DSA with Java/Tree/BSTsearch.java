84c6967f62fed5b33145f1fdd85a5cd1ebf12fd6
Chandrahas10 <chandrahaskori9@gmail.com>
2026-06-21 11:12:20 +0530

    search in the BST

---
+++ DSA with Java/Tree/BSTsearch.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Node{
+    int data;
+    Node left,right;
+
+    Node(int value){
+        data=value;
+        left = right = null;
+    }
+}
+public class BSTsearch {
+ static Scanner sc =new Scanner(System.in);
+    public static void main(String [] args){
+        Node root;
+        root =null;
+        
+
+    }
+    public static Node BSTsearch(){
+
+    }
+
+}
